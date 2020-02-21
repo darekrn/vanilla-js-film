76a52cef29ee17e164966858e8c077fa73002ac1
darekrn <60115098+darekrn@users.noreply.github.com>
2020-02-21 20:01:12 +0000

    flexbox + some styles

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,38 @@
-import leftPad from './leftpad';
+// import leftPad from './leftpad';
 
-const serNos = [6934, 23111, 23114, 1001, 211161];
-const partEl = document.getElementById('part-list');
-const strList = serNos.reduce(
-  (acc, element) => acc += `<li>${leftPad(element, 8, '0')}</li>`, ''
-);
-console.log(strList)
-partEl.innerHTML = strList;
+// const serNos = [6934, 23111, 23114, 1001, 211161];
+// const partEl = document.getElementById('part-list');
+// const strList = serNos.reduce(
+//   (acc, element) => acc += `<li>${leftPad(element, 8, '0')}</li>`, ''
+// );
+// console.log(strList)
+// partEl.innerHTML = strList;
+
+import HeaderView from './components/HeaderView'
+import PubSub from './pubsub.js'
+
+// class Header {
+//   constructor(element) {
+//     this.element = element
+//   }
+//   render() {
+//     var para = document.createElement("P");                       // Create a <p> node
+//     var t = document.createTextNode("This is a paragraph.");      // Create a text node
+//     para.appendChild(t);                                          // Append the text to <p>
+//     document.getElementById(this.element).appendChild(para);
+//   }
+// }
+
+//const dupa = new Header("middle")
+//dupa.render()
+const ps = new PubSub()
+const headerView = new HeaderView('header', ps)
+headerView.Render()
+
+class Store {
+  constructor() {
+    this.state = {}
+    this.actions = {}
+    this.mutations = {}
+  }
+}
---
+++ src/js/pubsub.js
@@ -0,0 +1,15 @@
+export default class PublisherSubscriber {
+    constructor(){
+
+    }
+
+    publish(notificationType, target, value) {
+
+    }
+
+    //observer - the method to be invoked
+    //source - element which is the source of the event
+    subscribe(notificationType, callback, argumen) {
+
+    }
+}
